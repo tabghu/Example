e0f9685d75f32167035f67e377beb8d3af32add4
tabghu <1538546920@qq.com>
2020-10-16 10:51:26 +0800

    测试

---
+++ 基于STL的演讲比赛流程管理系统/基于STL的演讲比赛流程管理系统/演讲比赛流程管理系统.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include "speechManager.h"
+#include "Speaker.h"
+using namespace std;
+
+int main()
+{
+	speechManager sm;
+
+	//test模块
+	/*for (map<int, Speaker>::iterator it = sm.m_Speaker.begin(); it != sm.m_Speaker.end(); it++)
+	{
+		cout << "选手编号" << it->first
+			<< " 姓名: " << it->second.m_Name
+			<< " 成绩： " << it->second.m_Score[0] << endl;
+	}*/
+	int choice = 0;		//用来存储用户的选项
+	while (true)
+	{
+		sm.show_Menu();
+		cout << "请输入您的选择： " << endl;
+		cin >> choice;		//接受用户的选项
+		switch (choice)
+		{
+		case 1:		//开始比赛
+			sm.startSpeech();
+			break;
+		case 2:		//查看记录
+			sm.showRecord();
+			break;
+		case 3:		//清空记录
+			sm.clearRecord();
+			break;
+		case 0:		//退出系统
+			sm.exitSystem();
+			break;
+		default:
+			system("cls");//清屏
+			break;
+		}
+	}
+	system("pause");
+	return 0;
+}
